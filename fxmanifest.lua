0d08281b706049a7c1ec27e1a25e452d26207810
Mantelis <mantsm0@gmail.com>
2026-02-19 13:32:06 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,18 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'CityRP Development'
-description 'CityRP Premium Loading Screen'
+author 'Mantelis Development'
+description 'Mantelis Premium Loading Screen'
 version '1.0.0'
 
 loadscreen 'html/index.html'
 loadscreen_manual_shutdown 'yes'
 
 files {
-    'html/index.html',
-    'html/style.css',
-    'html/script.js',
-    'html/config.js',
+    'index.html',
+    'style.css',
+    'script.js',
+    'config.js',
 }
 
 -- Jei nori rankinio uzdarymo (pvz. po NUI callbacko), naudok:
